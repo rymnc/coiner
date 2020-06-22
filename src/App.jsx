256de3c6f3075f960fd9a7553de2bd70044cebf0
aaryamannchallani <aaryamannchallani7@gmail.com>
2020-06-22 14:29:55 +0530

    Initial Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './bootstrap.min.css'
+import './App.css'
+import { BrowserRouter,Route } from 'react-router-dom';
+import CoinDetailPage from './pages/CoinDetailPage'
+import CoinSummaryPage from './pages/CoinSummaryPage'
+import Header from './components/Header';
+
+
+
 
 const App = () => {
     return (
-        <div>
-            Test
-        </div>
+        <BrowserRouter>
+        <Header/>
+         <Route exact path='/'>
+         <CoinSummaryPage/>
+             </Route>          
+           
+        </BrowserRouter>
     )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <h1 className="display-4 text-center mt-3 mb-4 text-white">
+           <span role="img" aria-label="boom">💥</span>  Coiner <span role="img" aria-label="boom2">💥</span>  
+        </h1>
+    )
+}
+
+export default Header
