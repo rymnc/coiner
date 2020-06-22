e208c0f2256210abaa3eb3b0dcb628342f604b52
aaryamannchallani <aaryamannchallani7@gmail.com>
2020-06-22 14:56:30 +0530

    Added coingecko api

---
+++ src/components/AddCoin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddCoin = () => {
+    return (
+        <div>
+            ac
+        </div>
+    )
+}
+
+export default AddCoin
---
+++ src/components/CoinList.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import coinGecko from '../apis/coinGecko'
+
+const CoinList = () => {
+
+    const [coins,setCoins] = useState([])
+
+    useEffect(()=>{
+        const fetchData = async () =>{
+
+            const data  = await coinGecko.get("/coins/markets",{
+                params:{
+                    vs_currency: "usd",
+                    ids: "bitcoin,ethereum"
+                }
+            })
+
+            console.log(data.data)
+        }
+
+        fetchData();
+    },[])
+
+
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CoinList
